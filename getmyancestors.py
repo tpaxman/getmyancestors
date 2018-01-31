4775c56f0be0fff79517f2376f5c29faf2f5efa4
Benoît Fontaine <benoitfontaine.ba@gmail.com>
2018-01-31 18:52:36 +0100

    Fix bug from last commit (when there isn't -i option)

--- getmyancestors.py
+++ getmyancestors.py
@@ -931,10 +931,10 @@ if __name__ == '__main__':
         parser.print_help()
         exit(2)
 
-    for fid in args.i:
-        if not re.match(r'[A-Z0-9]{4}-[A-Z0-9]{3}', fid):
-            print('Invalid FamilySearch ID: ' + fid)
-            exit(2)
+    if args.i:
+        for fid in args.i:
+            if not re.match(r'[A-Z0-9]{4}-[A-Z0-9]{3}', fid):
+                exit('Invalid FamilySearch ID: ' + fid)
 
     username = args.u if args.u else input("Enter FamilySearch username: ")
     password = args.p if args.p else getpass.getpass("Enter FamilySearch password: ")
