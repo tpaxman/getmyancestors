dd7fffbd68aecc99e462dd78f68f33779280e363
Tyler Paxman (laptop) <tpaxman@ualberta.ca>
2018-11-29 12:20:41 -0700

    got OxyGen class to work extending the pd.DataFrame

--- csv_to_hierarchy.py
+++ csv_to_hierarchy.py
@@ -8,29 +8,148 @@ import pandas as pd
 import numpy as np
 
 
-# These are the column names of the wanted variables, produced by Oxy-Gen, with their conversions to easier names:
+# # CLASS
 
 # In[ ]:
 
 
-keep_dict = {
-    'OXG_ID_INDIVIDU'  : 'person',
-    'OXG_ID_FATHER'    : 'father',
-    'OXG_ID_MOTHER'    : 'mother',
-    'OXG_PARTNER'      : 'spouse',
-    'OXG_ID_PATRONYME' : 'surname',
-    'OXG_GIVNAME'      : 'firstname',
-    'OXG_Sex'          : 'sex',
-}
+class OxyGen(pd.DataFrame):
+    
+    def  __init__(self, filename):
+        KEEP_DICT = {
+        'OXG_ID_INDIVIDU'  : 'person',
+        'OXG_ID_FATHER'    : 'father',
+        'OXG_ID_MOTHER'    : 'mother',
+        'OXG_PARTNER'      : 'spouse',
+        'OXG_ID_PATRONYME' : 'surname',
+        'OXG_GIVNAME'      : 'firstname',
+        'OXG_Sex'          : 'sex',
+        }
+        SEX_DICT = {0:'male',1:'female'}
+        df = pd.read_csv(filename)
+        df = df[list(KEEP_DICT.keys())]
+        df.rename(columns=KEEP_DICT,inplace=True)
+        df['sex'].replace(SEX_DICT, inplace=True)
+        df.set_index('person',inplace=True)
+        super().__init__(df)
+        
+    def find_origin_couple(self):
+        df = self.df
+        people_with_no_parents = df[np.isnan(df.mother) & np.isnan(df.father)].index
+        for person in people_with_no_parents:
+            spouse = df.spouse[person]
+            spousemother = df.mother[spouse]
+            spousefather = df.father[spouse]
+            if np.isnan([spousemother,spousefather]).all():
+                if df.sex[person]=='male':
+                    origin_couple = {'male':person, 'female':spouse}
+                elif df.sex[person=='female']:
+                    origin_couple = {'male':spouse, 'female':person}
+                break
+        return origin_couple
+    
+    def find_children(self,father=None,mother=None):
+        df = self.df
+        
+        children = df[(df.father==father) & (df.mother==mother)].index.tolist()
+        return self.convert_to_int(children)
+
+    def find_spouse(self,person):
+        df = self.df
+        spouse = df.spouse[person]
+        return self.convert_to_int(spouse)
 
 
 # In[ ]:
 
 
-df = pd.read_csv('./test3_familytreebeard/descend_familytreebeard.csv')
-df = df[list(keep_dict.keys())]
-df.rename(columns=keep_dict,inplace=True)
-df['sex'].replace({0:'male',1:'female'}, inplace=True)
+dfx=OxyGen(TEST_FILE)
+
+
+# In[ ]:
+
+
+dfx[dfx.father==dfx.father]
+
+
+# # TESTS
+
+# In[ ]:
+
+
+TEST_FILE = './test3_familytreebeard/descend_familytreebeard.csv'
+og = OxyGen(TEST_FILE)
+
+
+# In[ ]:
+
+
+def convert_to_int(values):
+    if np.isnan(values).any():
+        return values
+    else:
+        int_values = [int(a) for a in values] # DOESN'T WORK IF VALUES ISN'T ITERABLE!? WHYYY
+        return int_values
+
+x = [float('nan'),2.0,3.0]
+y = [1.0,2.0,3.0]
+convert_to_int(1)
+
+
+# ## Class Instance
+
+# In[ ]:
+
+
+[int(x) for x in [1.,2.,3.]]
+
+
+# In[ ]:
+
+
+ged_actual.find_origin_couple()
+
+
+# In[ ]:
+
+
+ged_actual.find_children(0,4)
+
+
+# In[ ]:
+
+
+ged_actual.find_spouse([1,2,4])
+
+
+# ## Sandbox
+
+# In[ ]:
+
+
+def find_spouse(df,person):
+    
+
+
+# In[ ]:
+
+
+def find_children(df,father,mother):
+    df[(df.father==father) & (df.mother==mother)].index.tolist()
+
+
+# In[ ]:
+
+
+df[(df.mother==4) & (df.father==0)].index.tolist()
+
+
+# In[ ]:
+
+
+#df = df[list(keep_dict.keys())]
+#df.rename(columns=keep_dict,inplace=True)
+#df['sex'].replace({0:'male',1:'female'}, inplace=True)
 
 df.set_index('person',inplace=True)
 
@@ -70,14 +189,6 @@ def find_children(df,father,mother):
     
 find_children(df,0,4)
     
-
-
-# In[ ]:
-
-
-class GedcomDF(pd.DataFrame):
-    def  __init__(self, filename):
-        self = pd.read_csv(filename)
 
 
 # In[ ]:
